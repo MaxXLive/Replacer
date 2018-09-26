e11cf3f57b59453f85db9089fa40037329a24e06
Max Ermackov <Maxermackov@live.de>
2018-09-26 23:35:12 +0200

    Add files via upload

--- Replacer/Replacer/Form1.Designer.cs
+++ Replacer/Replacer/Form1.Designer.cs
@@ -40,6 +40,11 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -62,33 +67,39 @@
             // 
             // textBox3
             // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.textBox3.Location = new System.Drawing.Point(12, 234);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(335, 20);
+            this.textBox3.Size = new System.Drawing.Size(270, 20);
             this.textBox3.TabIndex = 4;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(357, 234);
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(288, 234);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(335, 20);
+            this.textBox4.Size = new System.Drawing.Size(277, 20);
             this.textBox4.TabIndex = 5;
             this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // label3
             // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(12, 218);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(78, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Replace / Find";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(354, 218);
+            this.label4.Location = new System.Drawing.Point(288, 218);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(26, 13);
             this.label4.TabIndex = 7;
@@ -96,9 +107,10 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button1.Location = new System.Drawing.Point(12, 470);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.Size = new System.Drawing.Size(98, 23);
             this.button1.TabIndex = 8;
             this.button1.Text = "Find";
             this.button1.UseVisualStyleBackColor = true;
@@ -106,9 +118,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(119, 470);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(112, 470);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.Size = new System.Drawing.Size(98, 23);
             this.button2.TabIndex = 9;
             this.button2.Text = "Copy Output";
             this.button2.UseVisualStyleBackColor = true;
@@ -116,7 +129,10 @@
             // 
             // richTextBox1
             // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.Location = new System.Drawing.Point(12, 25);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(680, 190);
@@ -126,7 +142,10 @@
             // 
             // richTextBox2
             // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox2.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox2.Location = new System.Drawing.Point(12, 273);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.ReadOnly = true;
@@ -136,19 +155,88 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(226, 470);
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(320, 470);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(101, 23);
+            this.button3.Size = new System.Drawing.Size(98, 23);
             this.button3.TabIndex = 12;
             this.button3.Text = "Reset";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Replace with",
+            "Line Syntax",
+            "In-Between",
+            "Line-in-Between",
+            "Line Filter",
+            "Line Filter (invert)"});
+            this.comboBox1.Location = new System.Drawing.Point(571, 233);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(568, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Mode";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 234);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(553, 20);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label6.Font = new System.Drawing.Font("Wingdings", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label6.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label6.Location = new System.Drawing.Point(662, 4);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 18);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "1";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(216, 470);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Reuse Output";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(704, 505);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
@@ -160,11 +248,12 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "The Replacer 1337";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -182,6 +271,11 @@
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Replacer/Replacer/Form1.cs
+++ Replacer/Replacer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -13,6 +14,9 @@ namespace Replacer
 {
     public partial class Form1 : Form
     {
+        private int ReplaceMode = 0;
+        private string input1 = "";
+
         public Form1()
         {
             InitializeComponent();
@@ -20,33 +24,145 @@ namespace Replacer
 
         private void richTextBox1_TextChanged_1(object sender, EventArgs e)
         {
-            richTextBox2.Text = replace(richTextBox1.Text, textBox3.Text, textBox4.Text);
+            textChanged();
         }
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            richTextBox2.Text = replace(richTextBox1.Text, textBox3.Text, textBox4.Text);
+            textChanged();
+            input1 = textBox3.Text;
         }
 
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
-            richTextBox2.Text = replace(richTextBox1.Text, textBox3.Text, textBox4.Text);
+            textChanged();
+        }
+
+        private void textChanged()
+        {
+            if (ReplaceMode == 0)
+            {
+                richTextBox2.Text = replace(richTextBox1.Text, textBox3.Text, textBox4.Text);
+            }
+            else if (ReplaceMode == 1)
+            {
+                using (StringReader reader = new StringReader(richTextBox1.Text))
+                {
+                    string output = "";
+                    string line;
+                    int orgnr = 0;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        orgnr++;
+                        output += replace(replace(textBox1.Text, "$l", line), "$n", orgnr.ToString()) + Environment.NewLine;
+                    }
+                    richTextBox2.Text = output;
+                }
+            }
+            else if (ReplaceMode == 2)
+            {
+                string output = GetStringInBetween(textBox3.Text, textBox4.Text, richTextBox1.Text, false, false);
+
+                if (output.Length > 0)
+                {
+                    richTextBox2.Text = output;
+                }
+                else
+                {
+                    richTextBox2.Text = "No match";
+                }
+
+            }
+            else if (ReplaceMode == 3)
+            {
+                if (textBox3.Text.Length > 0 && textBox4.Text.Length > 0)
+                {
+                    using (StringReader reader = new StringReader(richTextBox1.Text))
+                    {
+                        string output = "";
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string temp = GetStringInBetween(textBox3.Text, textBox4.Text, line, false, false);
+                            if(temp.Length > 0)
+                            {
+                                output += temp + Environment.NewLine;
+                            }
+                            
+                        }
+
+                        if(output.Length > 0)
+                        {
+                            richTextBox2.Text = output;
+                        }
+                        else
+                        {
+                            richTextBox2.Text = "No match";
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    richTextBox2.Text = richTextBox1.Text;
+                }
+            }
+            else if (ReplaceMode == 4)
+            {
+                using (StringReader reader = new StringReader(richTextBox1.Text))
+                {
+                    string output = "";
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.Contains(textBox1.Text))
+                        {
+                            output += line + Environment.NewLine;
+                        }
+                    }
+
+                    if (output.Length > 0)
+                    {
+                        richTextBox2.Text = output;
+                    }
+                    else
+                    {
+                        richTextBox2.Text = "No match";
+                    }
+                }
+            }
+            else if (ReplaceMode == 5)
+            {
+                using (StringReader reader = new StringReader(richTextBox1.Text))
+                {
+                    string output = "";
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (!(line.Contains(textBox1.Text)))
+                        {
+                            output += line + Environment.NewLine;
+                        }
+                    }
+
+                    if (output.Length > 0)
+                    {
+                        richTextBox2.Text = output;
+                    }
+                    else
+                    {
+                        richTextBox2.Text = "No match";
+                    }
+                }
+            }
         }
 
         public string replace(string source, string replace, string with)
         {
             button1.Text = "Find";
-            if (replace != "" && with != "")
+            if (replace != "")
             {
-                if (with != "-1")
-                {
-                    string output = source.Replace(replace, with);
-                    return output;
-                }
-                else
-                {
-                    string output = source.Replace(replace, "");
-                    return output;
-                }
+                string output = source.Replace(replace, with);
+                return output;
 
             }
             else
@@ -57,10 +173,40 @@ namespace Replacer
 
         }
 
+        public static string GetStringInBetween(string strBegin, string strEnd, string strSource, bool includeBegin, bool includeEnd)
+        {
+            string[] result = { string.Empty, string.Empty };
+            int iIndexOfBegin = strSource.IndexOf(strBegin);
+
+            if (iIndexOfBegin != -1)
+            {
+                if (includeBegin)
+                    iIndexOfBegin -= strBegin.Length;
+
+                strSource = strSource.Substring(iIndexOfBegin + strBegin.Length);
+
+                int iEnd = strSource.IndexOf(strEnd);
+                if (iEnd != -1)
+                {
+                    if (includeEnd)
+                        iEnd += strEnd.Length;
+                    result[0] = strSource.Substring(0, iEnd);
+                    if (iEnd + strEnd.Length < strSource.Length)
+                        result[1] = strSource.Substring(iEnd + strEnd.Length);
+                }
+            }
+            else
+                result[1] = strSource;
+            return result[0];
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Clipboard.SetText(richTextBox2.Text);
-            resetAll();
+            if(richTextBox2.Text.Length > 0)
+            {
+                System.Windows.Forms.Clipboard.SetText(richTextBox2.Text);
+                resetAll();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -96,11 +242,159 @@ namespace Replacer
             richTextBox2.Text = null;
             textBox3.Text = null;
             textBox4.Text = null;
+            textBox1.Text = null;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             resetAll();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1_Resize(sender, e);
+            comboBox1.SelectedIndex = comboBox1.FindStringExact("Replace with");
+
+            ToolTip toolTip1 = new ToolTip();
+            toolTip1.SetToolTip(label6, "Read from Textfile");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ReplaceMode = comboBox1.SelectedIndex;
+            textBox3.Text = input1;
+            textBox1.Text = input1;
+
+            if (ReplaceMode == 0)
+            {
+                label3.Text = "Replace / Find";
+                label4.Show();
+                label4.Text = "with";
+                textBox3.Show();
+                textBox4.Show();
+                textBox1.Hide();
+            }
+            else if (ReplaceMode == 1)
+            {
+                label3.Text = "Line Syntax (click for variables)";
+                label4.Hide();
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox1.Show();
+            }
+            else if (ReplaceMode == 2)
+            {
+                label3.Text = "Between this";
+                label4.Show();
+                label4.Text = "and that";
+                textBox3.Show();
+                textBox4.Show();
+                textBox1.Hide();
+            }
+            else if (ReplaceMode == 3)
+            {
+                label3.Text = "Between this";
+                label4.Show();
+                label4.Text = "and that";
+                textBox3.Show();
+                textBox4.Show();
+                textBox1.Hide();
+            }
+            else if (ReplaceMode == 4)
+            {
+                label3.Text = "Show line only if contains";
+                label4.Hide();
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox1.Show();
+            }
+            else if (ReplaceMode == 5)
+            {
+                label3.Text = "Show line only if not contains";
+                label4.Hide();
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox1.Show();
+            }
+
+            textChanged();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            input1 = textBox1.Text;
+            textChanged();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int width = this.Width;
+            int height = this.Height;
+
+            this.MinimumSize = new Size(350, 400);
+
+            label3.Location = new Point(label3.Location.X, (height - 120) / 2);
+            textBox3.Location = new Point(textBox3.Location.X, label3.Location.Y + 15);
+            textBox3.Width = (width - 171) / 2;
+            textBox1.Location = new Point(textBox3.Location.X, label3.Location.Y + 15);
+            richTextBox1.Height = label3.Location.Y - richTextBox1.Location.Y - 3;
+            richTextBox1.Width = width - 40;
+            label2.Location = new Point(label2.Location.X, textBox3.Location.Y + textBox3.Height + 5);
+            richTextBox2.Location = new Point(richTextBox2.Location.X, label2.Location.Y + label2.Height + 5);
+            richTextBox2.Height = height - richTextBox2.Location.Y - 90;
+            richTextBox2.Width = width - 40;
+            comboBox1.Location = new Point(width - comboBox1.Width - 30, textBox1.Location.Y);
+            label5.Location = new Point(comboBox1.Location.X, comboBox1.Location.Y - 15);
+            textBox4.Location = new Point(textBox3.Location.X + textBox3.Width + 5, textBox3.Location.Y);
+            label4.Location = new Point(textBox4.Location.X, textBox4.Location.Y - 15);
+            textBox4.Width = comboBox1.Location.X - textBox4.Location.X - 5;
+            textBox1.Width = comboBox1.Location.X - textBox1.Location.X - 5;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "Text files|*.txt|All files|*.*";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = theDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            StreamReader reader = new StreamReader(myStream);
+                            richTextBox1.Text = reader.ReadToEnd();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (richTextBox2.Text.Length > 0)
+            {
+                string temp = richTextBox2.Text;
+                resetAll();
+                richTextBox1.Text = temp;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if(ReplaceMode == 1)
+            {
+                string info = "$l - Original Line" + Environment.NewLine +
+                              "$n - Line Nr." + Environment.NewLine;
+                MessageBox.Show(info, "Variables");
+            }
         }
     }
 }
